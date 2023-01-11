85bc6f0865006e15c6539fe623f8bc2d26c11a7d
l04m33@gmail.com <l04m33@gmail.com>
2023-01-11 11:37:37 +0000

    More modal keys
    FossilOrigin-Name: 221a9397996e724617e20e7c6006cdb4dfdb02ff9130348c3f5a3e1e8ee65a8f

--- init.el
+++ init.el
@@ -28,50 +28,152 @@
   (interactive)
   (deactivate-mark))
 
+(defun init/open-lines-below (count)
+  "Open COUNT lines below."
+  (interactive "p")
+  (end-of-line)
+  (dotimes (_ count)
+    (electric-newline-and-maybe-indent)))
+
+(defun init/open-lines-above (count)
+  "Open COUNT lines above."
+  (interactive "p")
+  (beginning-of-line)
+  (dotimes (_ count)
+    (newline)
+    (forward-line -1)))
+
+(defun init/select-lines (count)
+  "Select COUNT lines from the current line."
+  (interactive "p")
+  (beginning-of-line)
+  (unless (use-region-p) (set-mark (point)))
+  (forward-line count))
+
+(defun init/select-line-before-point ()
+  (interactive)
+  (set-mark (point))
+  (beginning-of-line))
+
+(defun init/select-line-after-point ()
+  (interactive)
+  (set-mark (point))
+  (end-of-line))
+
+(defun init/select-to-next-line ()
+  (interactive)
+  (unless (use-region-p) (set-mark (point)))
+  (next-line))
+
+(defun init/select-to-prev-line ()
+  (interactive)
+  (unless (use-region-p) (set-mark (point)))
+  (previous-line))
+
+(defun init/select-to-next-word ()
+  (interactive)
+  (unless (use-region-p) (set-mark (point)))
+  (forward-word))
+
+(defun init/select-to-prev-word ()
+  (interactive)
+  (unless (use-region-p) (set-mark (point)))
+  (backward-word))
+
+(defun init/kill-selection ()
+  (interactive)
+  (if (use-region-p)
+    (kill-region (region-beginning) (region-end))
+    (progn
+      (set-mark (point))
+      (forward-char)
+      (kill-region (region-beginning) (region-end))
+      (deactivate-mark))))
+
+(defun init/kill-ring-save-selection ()
+  (interactive)
+  (if (use-region-p)
+    (kill-ring-save (region-beginning) (region-end))
+    (progn
+      (set-mark (point))
+      (forward-char)
+      (kill-ring-save (region-beginning) (region-end))
+      (exchange-point-and-mark)
+      (deactivate-mark))))
+
+
 (add-hook 'prog-mode-hook #'(lambda () (ryo-modal-mode 1)))
 
 (global-set-key (kbd "C-z") 'ryo-modal-mode)
+
 (ryo-modal-keys
+
  ("z" ryo-modal-mode)
+
  ("SPC"
   (("h" help-command :name "Help")))
+
  (":" execute-extended-command)
+
  ("." ryo-modal-repeat)
+
  ("m" backward-char)
+ ("i" forward-char)
  ("n" next-line)
  ("e" previous-line)
- ("i" forward-char)
+
+ ("M" init/select-line-before-point)
+ ("I" init/select-line-after-point)
+ ("N" init/select-to-next-line)
+ ("E" init/select-to-prev-line)
+
  ("w" forward-word)
  ("b" backward-word)
+
+ ("W" init/select-to-next-word)
+ ("B" init/select-to-prev-word)
+
  ("g"
   (("m" back-to-indentation)
    ("M" move-beginning-of-line)
    ("i" move-end-of-line)
-   ("e" backward-paragraph)
    ("n" forward-paragraph)
+   ("e" backward-paragraph)
+
    ("w" forward-sentence)
    ("b" backward-sentence)
+
    ("g" beginning-of-buffer)
    ("G" end-of-buffer)
    ("t" beginning-of-buffer)
    ("b" end-of-buffer)))
+
  ("f" isearch-forward)
  ("M-f" isearch-backward)
+
  ("v" set-mark-command)
  (";" init/deactivate-mark)
  ("M-;" exchange-point-and-mark)
- ("y" kill-ring-save)
- ("d" kill-region)
+ ("x" init/select-lines)
+
+ ("y" init/kill-ring-save-selection)
+ ("d" init/kill-selection)
  ("p" yank)
  ("u" undo)
+
  ("a" forward-char :exit t)
  ("A" move-end-of-line :exit t)
  ("Z" back-to-indentation :exit t)
+ ("o" init/open-lines-below :exit t)
+ ("O" init/open-lines-above :exit t)
+
  ("C-m" windmove-left)
  ("C-n" windmove-down)
  ("C-e" windmove-up)
  ("C-i" windmove-right))
+
 (define-key ryo-modal-mode-map (kbd "SPC x") ctl-x-map)
+
 
 (ryo-modal-keys
  (:norepeat t)
