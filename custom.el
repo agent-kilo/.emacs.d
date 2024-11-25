75421bd0f3f4a71aad02d97e8ac29f3ac25d6dca
k <>
2024-11-25 08:12:27 +0000

    uxntal-mode
    FossilOrigin-Name: 6310c314695df227c2654014130c2f21ed819426b45c311a4a4c81d3768c26d0

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
    '("6fc9e40b4375d9d8d0d9521505849ab4d04220ed470db0b78b700230da0a86c1" default))
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(zig-mode kotlin-mode markdown-mode ement vterm vc-fossil eglot citre imenu-anywhere sml-modeline phi-search multiple-cursors expand-region which-key color-theme-sanityinc-tomorrow kkp multistate use-package)))
+   '(uxntal-mode zig-mode kotlin-mode markdown-mode ement vterm vc-fossil eglot citre imenu-anywhere sml-modeline phi-search multiple-cursors expand-region which-key color-theme-sanityinc-tomorrow kkp multistate use-package)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
